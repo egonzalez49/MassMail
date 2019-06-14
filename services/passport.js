cad17af391643ada9be82e12c10bde09c663fa38
egonzalez49 <egonzalez49@gatech.edu>
2019-06-14 17:42:00 -0400

    Proxy Change

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     //callback function after user acceps auth
     (accessToken, refreshToken, profile, done) => {
